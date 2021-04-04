9ddb47372239c5c3c46d25ef41566f73fc4c9375
Manit Singhal <76865274+04manit@users.noreply.github.com>
2021-04-04 09:24:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function changecolorheading() {
+    document.getElementById("heading").style.color = 'red';
+}
+function changeparacolor() {
+    document.getElementById("para").style.color = 'green';
+}
